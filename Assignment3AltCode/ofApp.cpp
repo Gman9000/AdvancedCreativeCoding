6b1efc5b0f4c664a9d40b97900c2ce02861c9cd4
Gman9000 <31265851+Gman9000@users.noreply.github.com>
2020-02-20 22:58:16 -0500

    Alternate Artwork
    Alternate Artwork made for Assignment 3

---
+++ Assignment3AltCode/ofApp.cpp
@@ -0,0 +1,81 @@
+#include "ofApp.h"
+#include "/Users/granv/Documents/of/of_v0.11.0_vs2017_release/apps/myApps/Assignment3/Shape.h"
+
+//--------------------------------------------------------------
+void ofApp::setup(){
+	ofSetWindowShape(1000, 1000);
+	ofSetFrameRate(60);
+}
+
+//--------------------------------------------------------------
+void ofApp::update(){
+}
+
+//--------------------------------------------------------------
+//runs every frame
+void ofApp::draw(){
+	for (Shape &s : shapes) {
+		s.draw();
+	}
+}
+
+//--------------------------------------------------------------
+void ofApp::keyPressed(int key){
+	//clears everything so you can try more variations
+	if (key == OF_KEY_RETURN) {
+		for (Shape &s : shapes) {
+			s.changeColor();
+		}
+	}
+
+}
+
+//--------------------------------------------------------------
+void ofApp::keyReleased(int key){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseMoved(int x, int y ){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseDragged(int x, int y, int button){
+	shapes.push_back(Shape());
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mousePressed(int x, int y, int button){
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseReleased(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseEntered(int x, int y){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseExited(int x, int y){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::windowResized(int w, int h){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::gotMessage(ofMessage msg){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::dragEvent(ofDragInfo dragInfo){ 
+
+}
