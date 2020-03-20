3f961db46b9d9aabee420c4be35e536d1d0c6131
Gman9000 <31265851+Gman9000@users.noreply.github.com>
2020-03-20 17:51:16 -0400

    Software Instruction 3
    New Software Instruction implemented

---
+++ SoftwareInstruction3Code/ofApp.cpp
@@ -0,0 +1,159 @@
+#include "ofApp.h"
+ofImage screenshottedImage;
+float angle = 0;
+
+//--------------------------------------------------------------
+void ofApp::setup() {
+	//background stuff
+	ofSetBackgroundAuto(false);
+	ofBackground(0, 0, 0);
+	
+	//Frame Rate Stuff
+	ofSetFrameRate(30);
+	//screen size
+	window_width = 1000;
+	window_height = 1000;
+	ofSetWindowShape(window_width, window_height);
+
+	//gui stuff
+	isDrawingGui = true;
+	gui.setup("X->hide|S->screenshot");
+	gui.add(lineColor.set("Line Color", ofColor(0, 255, 255), ofColor(0), ofColor(255)));
+	//gui.add(lineThickness.set("Line Thickness", 1, 1, 20));
+	gui.add(numberOfPoints.set("number of points", 100, 100, 200));
+	//gui.add(backGroundColor.set("Background Color", ofColor(0, 0, 0), ofColor(0), ofColor(255)));
+	gui.add(timeToChangePoints.set("Toggle Points", false));
+	gui.add(screenShot.set("Screenshot"));
+
+	//point stuff
+	origin = ofVec2f(window_width, window_height);
+	for (int i = 0; i < numberOfPoints; i++) {
+		int randomX = ofRandom(0, window_width);
+		int randomY = ofRandom(0, window_height);
+		points.push_back(new ofVec2f(randomX, randomY));
+	}
+	currentNumberOfPoints = numberOfPoints;
+}
+
+//--------------------------------------------------------------
+void ofApp::update() {
+	startAngle += 0.015;
+	angle = startAngle;
+
+	if ((window_width != ofGetWidth()) || (window_height != ofGetHeight()) || (currentNumberOfPoints != numberOfPoints)) {
+		points.clear();
+		window_width = ofGetWindowWidth();
+		window_height = ofGetWindowHeight();
+		currentNumberOfPoints = numberOfPoints;
+		for (int i = 0; i < numberOfPoints; i++) {
+			int randomX = ofRandom(0, window_width);
+			int randomY = ofRandom(0, window_height);
+			points.push_back(new ofVec2f(randomX, randomY));
+		}
+	}
+	if (timeToChangePoints) {
+		timeToChangePoints.set("Toggle Points", false);
+		points.clear();
+		window_width = ofGetWindowWidth();
+		window_height = ofGetWindowHeight();
+		for (int i = 0; i < numberOfPoints; i++) {
+			int randomX = ofRandom(0, window_width);
+			int randomY = ofRandom(0, window_height);
+			points.push_back(new ofVec2f(randomX, randomY));
+		}
+	}
+
+}
+
+//--------------------------------------------------------------
+void ofApp::draw() {
+	//ofBackground(backGroundColor);
+	ofSetColor(0, 0, 0, 5);
+	ofDrawRectangle(0, 0, window_width, window_height);
+	/*if (ofGetFrameNum() % 30 == 0) {
+		// draws a black background every 10 frames
+		ofSetColor(0, 0, 0, 125);
+		ofDrawRectangle(0, 0, window_width, window_height);
+		//ofBackground(0, 0, 0);
+		//ofSetBackgroundColor(0, 0, 0, 125);
+	}*/
+	if (isDrawingGui) {
+		gui.draw();
+	}
+	for (vector<ofVec2f*>::iterator itr = points.begin(); itr != points.end(); ++itr) {
+		float y = ofMap(sin(angle), -1, 1, 0, float(ofGetWindowHeight()));
+		float x = ofMap(cos(angle), -1, 1, 0, float(ofGetWindowWidth()));
+		ofSetColor(lineColor);
+		origin = ofVec2f(x, y);
+		//ofSetLineWidth(lineThickness);
+		ofDrawLine((**itr), origin);
+		angle += angleVar;
+	}
+}
+
+//--------------------------------------------------------------
+void ofApp::keyPressed(int key) {
+	if (key == 'x') {
+		isDrawingGui = !isDrawingGui;
+	}
+	else if (key == 's') {
+		screenshottedImage.grabScreen(0, 0, ofGetWidth(), ofGetHeight());
+		screenshottedImage.save(screenShot.toString() + ".png");
+		screenShot.set("Screenshot Saved!");
+	}
+	//this is for clearing the screen purposes (developer functionality only)
+	else if (key = 'f') {
+		ofSetColor(0, 0, 0);
+		ofDrawRectangle(0, 0, window_width, window_height);
+	}
+}
+
+//--------------------------------------------------------------
+void ofApp::keyReleased(int key) {
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseMoved(int x, int y) {
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseDragged(int x, int y, int button) {
+	//origin = ofVec2f(x, y);
+}
+
+//--------------------------------------------------------------
+void ofApp::mousePressed(int x, int y, int button) {
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseReleased(int x, int y, int button) {
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseEntered(int x, int y) {
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseExited(int x, int y) {
+
+}
+
+//--------------------------------------------------------------
+void ofApp::windowResized(int w, int h) {
+
+}
+
+//--------------------------------------------------------------
+void ofApp::gotMessage(ofMessage msg) {
+
+}
+
+//--------------------------------------------------------------
+void ofApp::dragEvent(ofDragInfo dragInfo) {
+
+}
